0425090a558eb6ef9dd5b016fac0881dffd1043a
Rukshona Rad <rukshona.rad@gmail.com>
2024-03-15 18:20:42 -0400

    Add animation to services

--- src/page/home/sections/service/serviceImg/ServiceImage.tsx
+++ src/page/home/sections/service/serviceImg/ServiceImage.tsx
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useEffect, useRef } from "react";
+import styled, { keyframes } from "styled-components";
 import { Tool } from "./types";
 import { Icon } from "../../../../../design-system/icon";
 import { Container } from "../../../../components";
-import { toolsFrontEnd, toolsBackend, tools } from "./data";
+import { tools } from "./data";
 import { Typography } from "../../../../../design-system/Typography";
 
 const ToolBarWrapper = styled(Container)`
-    width: 100rem;
-    padding: 4rem 3rem;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 3rem;
-    align-items: center;
-    justify-content: space-around;
-    background-color: white;
-    border-radius: var(--border-radius-80);
-    margin-bottom: var(--space-30);
-    &:hover {
-        box-shadow: var(--shadow-3xl);
-        transition: all 0.2s;
+    width: 100%;
+    overflow: hidden;
+`;
+
+const scrollRightToLeft = keyframes`
+    0% {
+        transform: translateX(0%);
+    }
+    100% {
+        transform: translateX(-100%);
     }
 `;
 
+const ToolIconsContainer = styled.div`
+    display: flex;
+    animation: ${scrollRightToLeft} 20s linear infinite;
+`;
+
 const StyledIcon = styled(Icon)`
-    margin-right: 1rem;
+    margin-left: 2rem;
     cursor: pointer;
     position: relative;
 `;
@@ -32,92 +34,62 @@ const StyledIcon = styled(Icon)`
 const ToolBox = styled.div`
     display: flex;
     align-items: center;
-    &:hover {
-        transform: scale(1.1);
-        transition: all 0.3s;
-    }
 `;
+
 const SkillTitle = styled.span`
     font-size: var(--font-size-18);
     font-weight: var(--font-weight-700);
     color: var(--gray-500);
 `;
-interface ToolBarProps {
-    toolsFrontEnd: Tool[];
-    toolsBackend: Tool[];
-    tools: Tool[];
-}
+
 const ToolbarTitle = styled(Typography)`
     color: var(--jaguar-700);
     display: flex;
     justify-content: center;
 `;
 
-const ToolBar: React.FC<ToolBarProps> = ({
-    toolsFrontEnd,
-    toolsBackend,
-    tools
-}) => {
+const ToolBar: React.FC<{ tools: Tool[] }> = ({ tools }) => {
+    const toolIconsRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const toolIconsContainer = toolIconsRef.current;
+        if (toolIconsContainer) {
+            const toolIconsWidth = toolIconsContainer.offsetWidth;
+            const toolIconsScrollWidth = toolIconsContainer.scrollWidth;
+            if (toolIconsWidth < toolIconsScrollWidth) {
+                toolIconsContainer.addEventListener(
+                    "animationiteration",
+                    () => {
+                        if (toolIconsContainer) {
+                            toolIconsContainer.appendChild(
+                                toolIconsContainer.children[0].cloneNode(true)
+                            );
+                            toolIconsContainer.style.transform = "none";
+                        }
+                    }
+                );
+            }
+        }
+    }, []);
+
     return (
-        <>
-            <ToolBarWrapper>
-                <ToolbarTitle variant="h6" weight="bold">
-                    Frontend
-                </ToolbarTitle>
-                {toolsFrontEnd.map((tool, idx) => (
-                    <ToolBox key={idx}>
-                        <StyledIcon
-                            height="4rem"
-                            width="5rem"
-                            iconName={tool.icon}
-                            onClick={() => {}}
-                        />
-                        <SkillTitle>{tool.title}</SkillTitle>
-                    </ToolBox>
-                ))}
-            </ToolBarWrapper>
-            <ToolBarWrapper>
-                <ToolbarTitle variant="h6" weight="bold">
-                    Backend
-                </ToolbarTitle>
-                {toolsBackend.map((tool, idx) => (
-                    <ToolBox key={idx}>
-                        <StyledIcon
-                            height="4rem"
-                            width="5rem"
-                            iconName={tool.icon}
-                            onClick={() => {}}
-                        />
-                        <SkillTitle>{tool.title}</SkillTitle>
-                    </ToolBox>
-                ))}
-            </ToolBarWrapper>
-            <ToolBarWrapper>
-                <ToolbarTitle variant="h6" weight="bold">
-                    Overlap
-                </ToolbarTitle>
+        <ToolBarWrapper>
+            <ToolIconsContainer ref={toolIconsRef}>
                 {tools.map((tool, idx) => (
                     <ToolBox key={idx}>
                         <StyledIcon
                             height="4rem"
-                            width="5rem"
+                            width="7rem"
                             iconName={tool.icon}
-                            onClick={() => {}}
                         />
                         <SkillTitle>{tool.title}</SkillTitle>
                     </ToolBox>
                 ))}
-            </ToolBarWrapper>
-        </>
+            </ToolIconsContainer>
+        </ToolBarWrapper>
     );
 };
 
 export const ToolsBar: React.FC = () => {
-    return (
-        <ToolBar
-            toolsFrontEnd={toolsFrontEnd}
-            toolsBackend={toolsBackend}
-            tools={tools}
-        />
-    );
+    return <ToolBar tools={tools} />;
 };
--- src/page/home/sections/service/serviceImg/data.ts
+++ src/page/home/sections/service/serviceImg/data.ts
@@ -1,23 +1,18 @@
 import { Tool } from "./types";
 
-export const toolsFrontEnd: Tool[] = [
+export const tools: Tool[] = [
+    { icon: "javascript", title: "JavaScript" },
+    { title: "TypeScript", icon: "typescript" },
     { title: "HTML", icon: "html" },
     { title: "React", icon: "react" },
     { title: "CSS", icon: "css" },
-    { title: "Figma", icon: "figma" }
-];
-export const toolsBackend: Tool[] = [
+    { title: "Figma", icon: "figma" },
+    { title: "Git", icon: "git" },
+    { title: "GitHub", icon: "github" },
+    { title: "Terminal", icon: "terminal" },
     { title: "Node.js", icon: "nodejs" },
     { title: "MongoDB", icon: "mongodb" },
     { title: "npm", icon: "npm" },
     { title: "VS Code", icon: "vscode" },
-
     { title: "Prisma", icon: "prisma" }
-];
-export const tools: Tool[] = [
-    { title: "JavaScript", icon: "javascript" },
-    { title: "TypeScript", icon: "typescript" },
-    { title: "Git", icon: "git" },
-    { title: "GitHub", icon: "github" },
-    { title: "Terminal", icon: "terminal" }
 ];
