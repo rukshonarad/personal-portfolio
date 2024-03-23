118c2c0369239f899aa3b7b3e0af67dc3bf76f63
Rukshona Rad <rukshona.rad@gmail.com>
2024-03-22 21:12:52 -0400

    Change input

--- src/design-system/Input/Input.tsx
+++ src/design-system/Input/Input.tsx
@@ -1,36 +1,22 @@
 import React from "react";
-import "./Input.css";
-import { trimWhiteSpaces } from "../utils/utils";
+import { sizeClassNames, shapeClassNames } from "./classNames";
 
 import { InputProps } from "./types";
-
-const sizeClassNames = {
-    sm: "input-small",
-    md: "input-medium",
-    lg: "input-large"
-};
-
-const shapeClassNames = {
-    rounded: "input-rounded",
-    circle: "input-circle"
-};
+import "./Input.css";
+import { trimWhiteSpaces } from "../utils/utils";
 
 const Input: React.FC<InputProps> = (props) => {
     const {
         type,
         error,
         disabled,
-        required,
         placeholder,
         shape,
         size,
         hintMessage,
-        labelText,
-        inputRef,
+
         className,
         id,
-        showPassword,
-        children,
         onChange,
         value
     } = props;
@@ -59,46 +45,27 @@ const Input: React.FC<InputProps> = (props) => {
     };
 
     return (
-        <div className={`input-wrapper ${className || ""} `}>
-            {(() => {
-                switch (type) {
-                    case "textarea":
-                        return (
-                            <textarea
-                                placeholder={placeholder}
-                                className={finalClassNames}
-                                required={required}
-                                disabled={disabled}
-                                id={id}
-                                onChange={handleOnChange}
-                                value={value}
-                                ref={
-                                    inputRef as React.LegacyRef<HTMLTextAreaElement>
-                                }
-                            />
-                        );
-                    default:
-                        return (
-                            <>
-                                <input
-                                    className={finalClassNames}
-                                    type={type || "text"}
-                                    placeholder={placeholder}
-                                    disabled={disabled}
-                                    required={required}
-                                    id={id}
-                                    onChange={handleOnChange}
-                                    value={value}
-                                    ref={
-                                        inputRef as React.LegacyRef<HTMLInputElement>
-                                    }
-                                />
-                                {showPassword ? children : null}
-                            </>
-                        );
-                }
-            })()}
-
+        <div className={`input-wrapper ${className || ""}`}>
+            {type === "textarea" ? (
+                <textarea
+                    placeholder={placeholder}
+                    className={finalClassNames}
+                    disabled={disabled}
+                    id={id}
+                    onChange={handleOnChange}
+                    value={value}
+                />
+            ) : (
+                <input
+                    className={finalClassNames}
+                    type={type || "text"}
+                    placeholder={placeholder}
+                    disabled={disabled}
+                    id={id}
+                    onChange={handleOnChange}
+                    value={value}
+                />
+            )}
             {hintMessage ? (
                 <span className={hintMessageClass}>{hintMessage}</span>
             ) : null}
--- src/design-system/Input/types.ts
+++ src/design-system/Input/types.ts
@@ -3,22 +3,18 @@ export type InputShape = "rounded" | "circle";
 export type InputSize = "sm" | "md" | "lg";
 
 export type InputProps = {
-    type?: "text" | "email" | "password" | "tel" | "textarea";
+    type?: InputType;
     disabled?: boolean;
-    required?: boolean;
     placeholder?: string;
     className?: string;
     id?: string;
-    showPassword?: boolean;
-    children?: React.ReactNode;
 
     error?: boolean;
-    shape?: "rounded" | "circle";
-    size?: "sm" | "md" | "lg";
+    shape?: InputShape;
+    size?: InputSize;
     hintMessage?: string;
-    labelText?: string;
-    inputRef?: React.RefObject<HTMLInputElement | HTMLTextAreaElement | null>;
 
     onChange: (value: string) => void;
     value: string;
+    name?: string;
 };
